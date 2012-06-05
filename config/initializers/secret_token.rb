bf098455b300c56475d8b90671abafe4d0a19029
Maurizio De Magnis <maurizio.demagnis@gmail.com>
2012-06-05 13:39:17 +0200

    Genesis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rubysocialclub::Application.config.secret_token = '26447e97f0fa5a60f7c718f816bf1b714f2d24772fbfaedc6ed49cf8685c1e3dfc649853489c412593d5414d6c01d807c201ecbf3c74cdd745aea580226e6b31'
