7426a7c713336c4b6337a24d4f7d790b016ae7c1
olistik <maurizio.demagnis@gmail.com>
2012-06-12 19:17:06 +0200

    Shows the attendees count in the events index

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -3,7 +3,11 @@ require 'uri'
 module EventsHelper
 
   def event_button(event, subscribed)
-    link_to event.start.to_date.to_s, event_topics_path(event), class: "btn btn-large #{subscribed ? 'btn-primary' : ''}"
+    label = event.start.to_date.to_s
+    if event.users.count > 0
+      label += content_tag(:span, event.users.count, class: 'badge badge-inverse')
+    end
+    link_to label.html_safe, event_topics_path(event), class: "btn btn-large #{subscribed ? 'btn-primary' : ''}"
   end
 
   def event_users_status(users)
